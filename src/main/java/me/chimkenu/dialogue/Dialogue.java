82fb1b2ab634e40e821b53d313428c5e02e2013a
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-05-01 13:07:56 +0800

    First commit

---
+++ src/main/java/me/chimkenu/dialogue/Dialogue.java
@@ -0,0 +1,17 @@
+package me.chimkenu.dialogue;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dialogue extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
