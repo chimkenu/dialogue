0646238b91c7cae5c0e8bb608f49695f945e2b80
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-05-06 00:13:06 +0800

    Added functionality to play method

--- src/main/java/me/chimkenu/dialogue/DialogueNode.java
+++ src/main/java/me/chimkenu/dialogue/DialogueNode.java
@@ -1,24 +1,50 @@
 package me.chimkenu.dialogue;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DialogueNode {
     private final TextComponent text;
+    public final int ticksPerChar;
     public DialogueNode next;
 
-    public DialogueNode(TextComponent text, DialogueNode next) {
+    public DialogueNode(TextComponent text, int ticksPerChar, DialogueNode next) {
         this.text = text;
+        this.ticksPerChar = ticksPerChar;
         this.next = next;
     }
 
     /**
-     * @param player The player that will see the dialogue
-     * @param delay  Time (in ticks) before the text plays
-     * @return       The number of ticks the text plays for
+     * @param player        the player that will see the dialogue
+     * @param delay         time (in ticks) before the text plays
+     * @param plugin        the reference to the plugin scheduling task
+     * @return              the number of ticks the text plays for
      */
-    public int play(Player player, int delay) {
-        // TODO: ADD FUNCTIONALITY
-        return 0;
+    public List<BukkitTask> play(Player player, int delay, JavaPlugin plugin) {
+        List<BukkitTask> tasks = new ArrayList<>();
+        int totalTime = 1; // adds a 1 tick delay to avoid text collisions
+
+        for (int i = 0; i < text.content().length(); i++) {
+            int temp = i;
+            tasks.add(new BukkitRunnable() {
+                @Override
+                public void run() {
+                    player.sendMessage(Component.text(text.content().substring(0, temp)));
+                }
+            }.runTaskLater(plugin, delay + totalTime));
+            totalTime += ticksPerChar;
+        }
+        return tasks;
+    }
+
+    public int getTotalTime() {
+        return 1 + (text.content().length() * ticksPerChar); // adds a 1 tick delay to avoid text collisions
     }
 }
