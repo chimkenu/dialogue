a239dc99c29ea58c5891eff0e8c3f452446a8da1
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-05-05 23:29:35 +0800

    Added onEnable logic

--- src/main/java/me/chimkenu/dialogue/Dialogue.java
+++ src/main/java/me/chimkenu/dialogue/Dialogue.java
@@ -1,17 +1,18 @@
 package me.chimkenu.dialogue;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+
 public final class Dialogue extends JavaPlugin {
+    public HashMap<Player, Location> playersInDialogue;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        playersInDialogue = new HashMap<>();
+        getCommand("dialogue").setExecutor(new DialogueCommand(playersInDialogue));
+        getServer().getPluginManager().registerEvents(new DialogueListener(playersInDialogue), this);
     }
 }
